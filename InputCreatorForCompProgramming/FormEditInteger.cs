dd148719f349e645cfd39d5c43d1e6dc3c3d2262
charon1212 <CharonS2turquoise@gmail.com>
2020-05-10 20:44:02 +0900

    dev #1 add:整数入力用フォームとUIを追加

---
+++ InputCreatorForCompProgramming/FormEditInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InputCreatorForCompProgramming
+{
+    public partial class FormEditInteger : Form
+    {
+        public FormEditInteger()
+        {
+            InitializeComponent();
+        }
+
+        private void rbDivisorCustom_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDivisorCustom.Enabled = rbDivisorCustom.Checked;
+        }
+    }
+}
