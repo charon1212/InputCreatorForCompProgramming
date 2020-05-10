969ddae586b422d8cf238c97786685e3768f3847
charon1212 <CharonS2turquoise@gmail.com>
2020-05-10 18:48:35 +0900

    dev #1 add:InputInfoの基底クラスを作成

---
+++ InputCreatorForCompProgramming/InputInfoBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputCreatorForCompProgramming
+{
+    abstract public class InputInfoBase
+    {
+        public string name { get; set; }
+        public InputType inputType { get; set; }
+        public InputInfoBase()
+        {
+            name = "";
+            inputType = InputType.None;
+        }
+
+        /// <summary>
+        /// InputInfoの設定をユーザーに伝える文字列を作成します。
+        /// </summary>
+        abstract public string makeDisplayText();
+    }
+
+    public enum InputType
+    {
+        None,
+        Integer,
+    }
+}
