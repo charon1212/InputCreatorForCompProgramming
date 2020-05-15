14fc2330a84220416840d418795bedbda4163223
charon1212 <CharonS2turquoise@gmail.com>
2020-05-16 00:01:31 +0900

    dev #6 add: ループ登録用のクラスを作成

---
+++ InputCreatorForCompProgramming/InputInfoIteratorEnd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputCreatorForCompProgramming
+{
+    public class InputInfoIteratorEnd : InputInfoBase
+    {
+        public InputInfoIteratorEnd() : base(InputType.IteratorEnd)
+        {
+        }
+        public override string makeDisplayText()
+        {
+            string text = "ループ終了";
+            return text;
+        }
+    }
+}
---
+++ InputCreatorForCompProgramming/InputInfoIteratorStart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputCreatorForCompProgramming
+{
+    public class InputInfoIteratorStart : InputInfoBase
+    {
+        int loopMin;
+        int loopMax;
+        public string divisorInter { get; }
+        public string divisorLast { get; }
+
+        public InputInfoIteratorStart(int loopMin, int loopMax, string divisorInter, string divisorLast) : base(InputType.IteratorStart)
+        {
+            this.loopMin = loopMin;
+            this.loopMax = loopMax;
+            this.divisorInter = divisorInter;
+            this.divisorLast = divisorLast;
+        }
+        public override string makeDisplayText()
+        {
+            string text = "ループ[" + loopMin.ToString() + "～" + loopMax.ToString() + "]";
+            return text;
+        }
+        public int getLoopCount(Random rnd, Dictionary<string, string> arg)
+        {
+            double r = rnd.NextDouble();
+            int result = loopMin + (int)Math.Floor(r * (loopMax - loopMin + 1));
+            return result;
+        }
+    }
+}
