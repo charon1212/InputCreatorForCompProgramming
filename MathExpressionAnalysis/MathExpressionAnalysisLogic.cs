5dde97fe468f4ba0566fcea5c2f1d68f8b794123
charon1212 <CharonS2turquoise@gmail.com>
2020-06-07 11:29:05 +0900

    Revert "dev #21 fix:バグを修正"
    This reverts commit 2a978e15cfbea41c63d100d55ac5600863e87d66.

--- MathExpressionAnalysis/MathExpressionAnalysisLogic.cs
+++ MathExpressionAnalysis/MathExpressionAnalysisLogic.cs
@@ -406,7 +406,7 @@ namespace MathExpressionAnalysis
                     leftLexicalList.Add(lexicalList[i]);
                 }
                 var rightLexicalList = new List<Lexical>();
-                for (int i = operatorIndex+1; i < lexicalList.Count; i++)
+                for (int i = 0; i < operatorIndex; i++)
                 {
                     rightLexicalList.Add(lexicalList[i]);
                 }
