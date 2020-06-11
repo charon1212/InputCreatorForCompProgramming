07cbc84332564785e3d9083e194bbb80a24a0085
charon1212 <CharonS2turquoise@gmail.com>
2020-06-12 06:25:05 +0900

    dev #8 add: 数式ツリーにデータ型チェック関数と評価関数を追加。将来的にはロジックからこっちに持ってくる予定。

--- MathExpressionAnalysis/Object/MathTree.cs
+++ MathExpressionAnalysis/Object/MathTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MathExpressionAnalysis.Object
@@ -13,5 +14,20 @@ namespace MathExpressionAnalysis.Object
         /// ルートノード。
         /// </summary>
         public MathTreeNode root { get; set; }
+
+        public MathTreeNodeValue eval(Dictionary<string, Variable> variableMap = null, Dictionary<string, Function> functionMap = null)
+        {
+            return MathExpressionAnalysisLogic.eval(this, variableMap, functionMap);
+        }
+        public DataType checkDataType(Dictionary<string, DataType> variableDataTypeMap = null,
+            Dictionary<string, DataType> functionDataTypeMap = null)
+        {
+            return MathExpressionAnalysisLogic.checkDataType(this, variableDataTypeMap, functionDataTypeMap);
+        }
+        public DataType checkDataType(Dictionary<string, Variable> variableMap,
+            Dictionary<string, Function> functionMap)
+        {
+            return MathExpressionAnalysisLogic.checkDataType(this, variableMap, functionMap);
+        }
     }
 }
